4635b39042be302d6dda9c095d6a3fdf51c2ace1
Syafiq Basri <altsyafiq@gmail.com>
2022-12-18 00:18:18 +0800

    :zap: Renamed config files and README project name to poket

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/piutang'),
+      dir: require('path').join(__dirname, './coverage/poket'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'piutang'`, () => {
+  it(`should have as title 'poket'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('piutang');
+    expect(app.title).toEqual('poket');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('piutang app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('poket app is running!');
   });
 });
