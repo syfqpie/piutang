91950facfbc9cf9cc672fabf5d6fd022d34eb396
Syafiq Basri <altsyafiq@gmail.com>
2022-12-19 20:13:19 +0800

    :construction: Improved light mode design

--- src/app/components/layouts/navbar-main/navbar-main.component.ts
+++ src/app/components/layouts/navbar-main/navbar-main.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
     <nav
 		class="bg-white border-gray-200 px-2
 		md:px-40 py-2.5 rounded dark:bg-gray-900
-		shadow-lg">
+		shadow-md dark:shadow-lg">
 		<div
 			class="container flex flex-wrap
 			items-center justify-between mx-auto">
@@ -14,9 +14,9 @@ import { Component, OnInit } from '@angular/core';
 				<button
 					type="button"
 					class="px-3 py-2 mr-3 rounded-lg border font-medium text-xs 
-					text-center text-emerald-700 border-emerald-700
-					hover:text-white hover:bg-emerald-800 focus:outline
-					focus:outline-2 focus:outline-emerald-300 dark:text-emerald-500
+					text-center text-emerald-700 border-emerald-600
+					hover:text-white hover:bg-emerald-700 focus:outline
+					focus:outline-2 focus:outline-emerald-200 dark:text-emerald-500
 					dark:border-emerald-500 dark:hover:text-white
 					dark:hover:bg-emerald-600 dark:focus:outline-emerald-800
 					disabled:cursor-not-allowed">
@@ -37,8 +37,8 @@ import { Component, OnInit } from '@angular/core';
 					[routerLink]="['/new']"
 					class="px-3 py-2 mr-3 md:mr-0 rounded-lg font-medium text-xs
 					border text-center text-white bg-emerald-700 hover:bg-emerald-800
-					focus:outline focus:outline-2 focus:outline-emerald-300
-					dark:bg-emerald-600 dark:hover:bg-emerald-700
+					focus:outline focus:outline-2 focus:outline-emerald-200
+					dark:bg-emerald-600 dark:hover:bg-emerald-700 focus:border-emerald-500
 					dark:focus:border-emerald-500 dark:border-emerald-600 
                	 	dark:focus:outline-emerald-800 disabled:cursor-not-allowed">
 					<i class="fa-solid fa-plus me-2"></i>
