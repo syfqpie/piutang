9d0fe515566c976929dbcafdaaa4003f6e75ba0f
Syafiq Basri <altsyafiq@gmail.com>
2023-01-08 14:36:01 +0800

    :construction: Added Latest section template

--- src/app/core/main/home/home.component.ts
+++ src/app/core/main/home/home.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { Debt, DebtType } from 'src/app/shared/services/debt/debt.model';
+import { DebtService } from 'src/app/shared/services/debt/debt.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  // Data
+  latestDebts: Debt[] = []
 
+  // Predfined
+  DebtType = DebtType
+
+  // Subscription
+  subscription: Subscription = new Subscription()
+
+  constructor(
+    private debtSvc: DebtService
+  ) { }
+                                     
   ngOnInit(): void {
+    this.getData()
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe()
+    }
+  }
+
+  getData() {
+    this.latestDebts = this.debtSvc.debts.slice(0, 5)
   }
 
 }
