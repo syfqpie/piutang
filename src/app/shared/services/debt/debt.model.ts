387e176ad6de810533490051b0608de507053615
Syafiq Basri <altsyafiq@gmail.com>
2023-01-08 21:15:43 +0800

    :construction: Updated Debt model

--- src/app/shared/services/debt/debt.model.ts
+++ src/app/shared/services/debt/debt.model.ts
@@ -1,16 +1,22 @@
+/**
+ * Debt base class
+ */
 export class Debt {
     constructor(
         public id: number,
         public amount: number,
         public notes: string,
         public type: DebtType,
-        public human: number,
-        public dueAt: Date | null,
-        public createdAt: Date,
-        public modifiedAt: Date
+        public human: string,
+        public dueAt: string | null,
+        public createdAt: string,
+        public modifiedAt: string
     ) {}
 }
 
+/**
+ * Enum for debt types
+ */
 export enum DebtType {
     LEND = 'Lend',
     BORROW = 'Borrow'
