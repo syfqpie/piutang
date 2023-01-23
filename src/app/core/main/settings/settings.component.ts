f7553e16dd3cdf96407405ddca24ee31d8e466cf
Syafiq Basri <altsyafiq@gmail.com>
2023-01-23 20:30:09 +0800

    :construction: Updated se ttings design

--- src/app/core/main/settings/settings.component.ts
+++ src/app/core/main/settings/settings.component.ts
@@ -1,23 +1,80 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from 'src/app/shared/services/auth/auth.service';
 import { Profile } from 'src/app/shared/services/profile/profile.model';
 import { ProfileService } from 'src/app/shared/services/profile/profile.service';
 
+interface SettingsConfig {
+	title: string,
+	description: string,
+	icon: string,
+	handler: () => void
+}
+
 @Component({
-  selector: 'app-settings',
-  templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.css']
+	selector: 'app-settings',
+	templateUrl: './settings.component.html',
+	styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
 
-  // Data
-  profile: Profile | null = null
+	// Data
+	profile: Profile | null = null
+	settings: SettingsConfig[] = [
+		{ 
+			title: 'Update name',
+			description: 'Update your name here',
+			icon: 'fa-regular fa-id-card',
+			handler: () => this.onUpdateName()
+		},
+		{ 
+			title: 'Change password',
+			description: 'Change your password',
+			icon: 'fa-solid fa-unlock-keyhole',
+			handler: () => this.onChangePassword() 
+		},
+		{ 
+			title: 'About',
+			description: 'App information',
+			icon: 'fa-solid fa-question',
+			handler: () => this.onAbout() 
+		},
+		{ 
+			title: 'Logout',
+			description: 'Sign out of system',
+			icon: 'fa-solid fa-arrow-right-from-bracket',
+			handler: () => this.onLogout() 
+		}
+	]
 
-  constructor(
-    private profileSvc: ProfileService
-  ) { }
+	constructor(
+		private router: Router,
+		private profileSvc: ProfileService,
+		private authSvc: AuthService
+	) { }
 
-  ngOnInit(): void {
-    this.profile = this.profileSvc.currentProfile
-  }
+	ngOnInit(): void {
+		this.profile = this.profileSvc.currentProfile
+	}
+
+	onUpdateName() {
+		console.log('onUpdateName')
+	}
+
+	onChangePassword() {
+		console.log('onChangePassword')
+	}
+
+	onAbout() {
+		console.log('onAbout')
+	}
+
+	onLogout() {
+		return this.authSvc.logout().then(() => { 
+			this.router.navigate(['/auth', 'login']) 
+		})
+	}
+	
 
 }
