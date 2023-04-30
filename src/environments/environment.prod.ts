8d4b66d21e9f95f48af7498b75459f6159eaab56
Syafiq Basri <altsyafiq@gmail.com>
2023-04-30 20:34:22 +0800

    ops: updated for release

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  appVersion: require('../../package.json').version,
+  baseDomain: 'http://localhost:4200/',
+  supabaseUrl: 'https://slgzkhfrhrkyxlswwpvo.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNsZ3praGZyaHJreXhsc3d3cHZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzMxNjM3NzEsImV4cCI6MTk4ODczOTc3MX0.p24FjoUFXjfgex7oDJ5vBCoU-vu5DnDx_bSwDFVOx0c'
 };
